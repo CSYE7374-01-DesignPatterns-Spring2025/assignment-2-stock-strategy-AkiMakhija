2b502f1ea759fc2db41851cc58da954166d76b3a
AkiMakhija <98291680+AkiMakhija@users.noreply.github.com>
2025-02-04 14:28:33 -0500

    Delete interfaces directory

--- interfaces/AbstractStockFactory.java
+++
@@ -1,9 +0,0 @@
-package edu.neu.csye7374.interfaces;
-
-import edu.neu.csye7374.Stock;
-
-public interface AbstractStockFactory {
-
-
-    Stock getObject();
-}
--- interfaces/PriceCalculationStrategy.java
+++
@@ -1,8 +0,0 @@
-package edu.neu.csye7374.interfaces;
-
-import java.util.List;
-
-public interface PriceCalculationStrategy {
-
-    double calculatePrice(List<Double> bid);
-}
